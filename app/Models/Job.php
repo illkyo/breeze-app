24320d1a254137252e7fb8e31ace5d3633580c63
illkyo <82893863+illkyo@users.noreply.github.com>
2025-07-16 14:02:31 +0500

    feat: migrations

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,34 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Admin',
-                'salary' => '2000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Boss',
-                'salary' => '5000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Owner',
-                'salary' => '10000'
-            ],
-        ];  
-    }
-
-    public static function find(int $id): array {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404);
-        };
-        return $job;
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
 }
