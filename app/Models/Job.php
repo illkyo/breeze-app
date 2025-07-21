0ac4cd4220940fd0a084babba7f2713b9623c100
illkyo <82893863+illkyo@users.noreply.github.com>
2025-07-21 13:29:31 +0500

    feat: establish relationship between employer, job

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Employer;
 
 class Job extends Model {
     use HasFactory;
+    
     protected $table = 'job_listings';
+    
     protected $fillable = ['title', 'salary'];
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
 }
