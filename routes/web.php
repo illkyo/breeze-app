b6b817ed2dcd0171bfba9b00843a8f93a438ece9
illkyo <82893863+illkyo@users.noreply.github.com>
2025-08-03 12:59:39 +0500

    feat: remove Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -53,15 +53,3 @@ Route::controller(ActivityController::class)->group(function () {
     Route::patch('/activities/{activity}', 'update');
     Route::delete('/activities/{activity}', 'delete');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
