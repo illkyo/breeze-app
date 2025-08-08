1c317b3711a98b0a5dfc0c7bdc77b27918c0aed0
illkyo <82893863+4thjonasbrother@users.noreply.github.com>
2025-08-08 22:42:21 +0500

    feat: configure vite bundling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,8 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+  @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body class="h-full">
 <div class="min-h-full">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,6 @@
   <x-slot:heading>
   Home
   </x-slot:heading>
-  <h1>{{ $greeting }} from the Home page, {{ $name }}</h1>  
+  <h1>{{ $greeting }} from the Home page, {{ $name }}</h1>
+  <button x-data @click="alert('I\'ve been clicked!')">Click Me</button>
 </x-layout>
